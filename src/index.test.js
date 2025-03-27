1855ae90c1b06dc92e58af47df52f85524cb0cf6
aish8la <64914670+aish8la@users.noreply.github.com>
2025-03-28 03:37:31 +0500

    test: add tests for reverseString function

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,23 @@
 import * as funcs from "./index.js";
 
-test("Capitalize first character of 'hello World'", () => {
+test("Capitalize first character string", () => {
     expect(funcs.capitalize('hello World')).toBe('Hello World');
 });
 
-test("Handles non string inputs and strings starting with, non alphabets and white spaces", () => {
+test("Capitlize: Handles non string inputs and strings starting with, non alphabets and white spaces", () => {
     expect(funcs.capitalize('   hello World ')).toBe('Hello World');
     expect(funcs.capitalize('{hello: world}')).toBe('{hello: world}');
     expect(funcs.capitalize("")).toBeUndefined();
     expect(funcs.capitalize({hello: "world"})).toBeUndefined();
+});
+
+test("Reverses string", () => {
+    expect(funcs.reverseString("Hello World")).toBe("dlroW olleH");
+})
+
+test("Handles non string inputs and strings starting with, non alphabets and white spaces", () => {
+    expect(funcs.reverseString('   Hello World ')).toBe(' dlroW olleH   ');
+    expect(funcs.reverseString('{hello: world}')).toBe('}dlrow :olleh{');
+    expect(funcs.reverseString("")).toBeUndefined();
+    expect(funcs.reverseString({hello: "world"})).toBeUndefined();
 });
