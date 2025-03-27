2d26401df8ea732abffbbc3cc4903cc1e94c04cb
aish8la <64914670+aish8la@users.noreply.github.com>
2025-03-28 03:38:49 +0500

    test: remove duplicate test

--- src/index.test.js
+++ src/index.test.js
@@ -2,9 +2,6 @@ import * as funcs from "./index.js";
 
 test("Capitalize first character string", () => {
     expect(funcs.capitalize('hello World')).toBe('Hello World');
-});
-
-test("Capitlize: Handles non string inputs and strings starting with, non alphabets and white spaces", () => {
     expect(funcs.capitalize('   hello World ')).toBe('Hello World');
     expect(funcs.capitalize('{hello: world}')).toBe('{hello: world}');
     expect(funcs.capitalize("")).toBeUndefined();
@@ -13,11 +10,9 @@ test("Capitlize: Handles non string inputs and strings starting with, non alphab
 
 test("Reverses string", () => {
     expect(funcs.reverseString("Hello World")).toBe("dlroW olleH");
-})
-
-test("Handles non string inputs and strings starting with, non alphabets and white spaces", () => {
     expect(funcs.reverseString('   Hello World ')).toBe(' dlroW olleH   ');
     expect(funcs.reverseString('{hello: world}')).toBe('}dlrow :olleh{');
     expect(funcs.reverseString("")).toBeUndefined();
     expect(funcs.reverseString({hello: "world"})).toBeUndefined();
 });
+
