c05b8efbcf4de72ba9c52ec2075d250a0bc5e52d
aish8la <64914670+aish8la@users.noreply.github.com>
2025-03-28 04:12:28 +0500

    feat: add multiply method

--- src/index.js
+++ src/index.js
@@ -25,5 +25,11 @@ export const calculator = {
         if(num1 === 0) return 0;
         if(typeof num1 !== "number" || typeof num2 !== "number" || num2 === 0) return
         return num1 / num2;
+    },
+
+    multiply: (num1, num2) => {
+        if(typeof num1 !== "number" || typeof num2 !== "number") return
+        if(num1 === 0 || num2 === 0) return 0;
+        return num1 * num2;
     }
 }
