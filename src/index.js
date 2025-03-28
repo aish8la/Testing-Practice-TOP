9b90c6a1c00a345e8e37a5e28ba709dca68f476f
aish8la <64914670+aish8la@users.noreply.github.com>
2025-03-28 08:16:25 +0500

    style: apply prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,65 +1,67 @@
 export function capitalize(text) {
-    if(typeof text !== "string") return
-    text = text.trim();
-    if(!text) return
-    return text.at(0).toUpperCase() + text.slice(1);
+  if (typeof text !== "string") return;
+  text = text.trim();
+  if (!text) return;
+  return text.at(0).toUpperCase() + text.slice(1);
 }
 
 export function reverseString(text) {
-    if(typeof text !== "string" || !text) return
-    return [...text].reverse().join("");
+  if (typeof text !== "string" || !text) return;
+  return [...text].reverse().join("");
 }
 
 export const calculator = {
-    add: (num1, num2) => {
-        if(typeof num1 !== "number" || typeof num2 !== "number") return
-        return num1 + num2;
-    },
+  add: (num1, num2) => {
+    if (typeof num1 !== "number" || typeof num2 !== "number") return;
+    return num1 + num2;
+  },
 
-    subtract: (num1, num2) => {
-        if(typeof num1 !== "number" || typeof num2 !== "number") return
-        return num1 - num2;
-    },
+  subtract: (num1, num2) => {
+    if (typeof num1 !== "number" || typeof num2 !== "number") return;
+    return num1 - num2;
+  },
 
-    divide: (num1, num2) => {
-        if(num1 === 0) return 0;
-        if(typeof num1 !== "number" || typeof num2 !== "number" || num2 === 0) return
-        return num1 / num2;
-    },
+  divide: (num1, num2) => {
+    if (num1 === 0) return 0;
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num2 === 0)
+      return;
+    return num1 / num2;
+  },
 
-    multiply: (num1, num2) => {
-        if(typeof num1 !== "number" || typeof num2 !== "number") return
-        if(num1 === 0 || num2 === 0) return 0;
-        return num1 * num2;
-    }
-}
+  multiply: (num1, num2) => {
+    if (typeof num1 !== "number" || typeof num2 !== "number") return;
+    if (num1 === 0 || num2 === 0) return 0;
+    return num1 * num2;
+  },
+};
 
 export const cipher = {
+  trueModulo(n, m) {
+    return ((n % m) + m) % m;
+  },
 
-    trueModulo(n, m) {
-        return ((n % m) + m) % m;
-    },
+  isUpper(char) {
+    return char === char.toUpperCase();
+  },
 
-    isUpper(char) {
-        return char === char.toUpperCase();
-    },
+  caesarCipher(text, shiftFact) {
+    let ciphered = "";
 
-    caesarCipher(text, shiftFact) {
-        let ciphered = '';
+    for (let i = 0; i < text.length; i++) {
+      const curChar = text.at(i);
 
-        for(let i = 0; i < text.length; i++) {
-            const curChar = text.at(i);
+      if (!/[a-zA-Z]/.test(curChar)) {
+        ciphered += curChar;
+        continue;
+      }
 
-            if(!(/[a-zA-Z]/.test(curChar))) {
-                ciphered += curChar;
-                continue;
-            }
-            
-            const baseCode = this.isUpper(curChar) ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0); //base character code based on the case
-            const charIndex = (curChar.charCodeAt(0) - baseCode); //index / how far current character is from base code
-            const shiftedIndex = this.trueModulo(charIndex + shiftFact, 26); // shift the char index based on the shift factor and apply true modulo to normalize negatives
-            ciphered += String.fromCharCode(shiftedIndex + baseCode); // add shifted index to base code to get the ciphered character
-        }
-        return ciphered;
-    },
-}
+      const baseCode = this.isUpper(curChar)
+        ? "A".charCodeAt(0)
+        : "a".charCodeAt(0); //base character code based on the case
+      const charIndex = curChar.charCodeAt(0) - baseCode; //index / how far current character is from base code
+      const shiftedIndex = this.trueModulo(charIndex + shiftFact, 26); // shift the char index based on the shift factor and apply true modulo to normalize negatives
+      ciphered += String.fromCharCode(shiftedIndex + baseCode); // add shifted index to base code to get the ciphered character
+    }
+    return ciphered;
+  },
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,65 +1,65 @@
 import * as funcs from "./index.js";
 
 test("Capitalize first character string", () => {
-    expect(funcs.capitalize('hello World')).toBe('Hello World');
-    expect(funcs.capitalize('   hello World ')).toBe('Hello World');
-    expect(funcs.capitalize('{hello: world}')).toBe('{hello: world}');
-    expect(funcs.capitalize("")).toBeUndefined();
-    expect(funcs.capitalize({hello: "world"})).toBeUndefined();
+  expect(funcs.capitalize("hello World")).toBe("Hello World");
+  expect(funcs.capitalize("   hello World ")).toBe("Hello World");
+  expect(funcs.capitalize("{hello: world}")).toBe("{hello: world}");
+  expect(funcs.capitalize("")).toBeUndefined();
+  expect(funcs.capitalize({ hello: "world" })).toBeUndefined();
 });
 
 test("Reverses string", () => {
-    expect(funcs.reverseString("Hello World")).toBe("dlroW olleH");
-    expect(funcs.reverseString('   Hello World ')).toBe(' dlroW olleH   ');
-    expect(funcs.reverseString('{hello: world}')).toBe('}dlrow :olleh{');
-    expect(funcs.reverseString("")).toBeUndefined();
-    expect(funcs.reverseString({hello: "world"})).toBeUndefined();
+  expect(funcs.reverseString("Hello World")).toBe("dlroW olleH");
+  expect(funcs.reverseString("   Hello World ")).toBe(" dlroW olleH   ");
+  expect(funcs.reverseString("{hello: world}")).toBe("}dlrow :olleh{");
+  expect(funcs.reverseString("")).toBeUndefined();
+  expect(funcs.reverseString({ hello: "world" })).toBeUndefined();
 });
 
 test("Calculator Object exists", () => {
-    expect(funcs.calculator).toBeDefined();
+  expect(funcs.calculator).toBeDefined();
 });
 
 test("Add two values", () => {
-    expect(funcs.calculator.add(2, 2)).toBe(4);
-    expect(funcs.calculator.add("hello", 4)).toBeUndefined();
+  expect(funcs.calculator.add(2, 2)).toBe(4);
+  expect(funcs.calculator.add("hello", 4)).toBeUndefined();
 });
 
 test("Subtract two values", () => {
-    expect(funcs.calculator.subtract(8, 4)).toBe(4);
-    expect(funcs.calculator.subtract("hello", 4)).toBeUndefined();
+  expect(funcs.calculator.subtract(8, 4)).toBe(4);
+  expect(funcs.calculator.subtract("hello", 4)).toBeUndefined();
 });
 
 test("Divide two values", () => {
-    expect(funcs.calculator.divide(12, 3)).toBe(4);
-    expect(funcs.calculator.divide(0, -3)).toBe(0);
-    expect(funcs.calculator.divide(12, 0)).toBeUndefined();
-    expect(funcs.calculator.divide("hello", 4)).toBeUndefined();
+  expect(funcs.calculator.divide(12, 3)).toBe(4);
+  expect(funcs.calculator.divide(0, -3)).toBe(0);
+  expect(funcs.calculator.divide(12, 0)).toBeUndefined();
+  expect(funcs.calculator.divide("hello", 4)).toBeUndefined();
 });
 
 test("Multiply two values", () => {
-    expect(funcs.calculator.multiply(2, 2)).toBe(4);
-    expect(funcs.calculator.multiply(0, 3)).toBe(0);
-    expect(funcs.calculator.multiply(-3, 0)).toBe(0);
-    expect(funcs.calculator.multiply("hello", 4)).toBeUndefined();
+  expect(funcs.calculator.multiply(2, 2)).toBe(4);
+  expect(funcs.calculator.multiply(0, 3)).toBe(0);
+  expect(funcs.calculator.multiply(-3, 0)).toBe(0);
+  expect(funcs.calculator.multiply("hello", 4)).toBeUndefined();
 });
 
 test("caesarCipher correctly ciphers", () => {
-    expect(funcs.cipher.caesarCipher('xyz', 3)).toBe('abc');
+  expect(funcs.cipher.caesarCipher("xyz", 3)).toBe("abc");
 });
 
 test("caesarCipher correctly outputs with same case", () => {
-    expect(funcs.cipher.caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  expect(funcs.cipher.caesarCipher("HeLLo", 3)).toBe("KhOOr");
 });
 
 test("caesarCipher correctly leaves punctuation unmodified", () => {
-    expect(funcs.cipher.caesarCipher('Hello, World!', 3)).toBe("Khoor, Zruog!");
+  expect(funcs.cipher.caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
 
 test("caesarCipher correctly handles negative shift factors", () => {
-    expect(funcs.cipher.caesarCipher('AB', -8)).toBe("ST");
+  expect(funcs.cipher.caesarCipher("AB", -8)).toBe("ST");
 });
 
 test("caesarCipher correctly handles large shift factors", () => {
-    expect(funcs.cipher.caesarCipher('AB', 50)).toBe("YZ");
+  expect(funcs.cipher.caesarCipher("AB", 50)).toBe("YZ");
 });
